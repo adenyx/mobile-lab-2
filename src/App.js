f0427bac64b2df32f5a6a7d2221b89f74f698270
adenyl <adenis124@gmail.com>
2022-04-17 19:05:44 +0300

    Network error handler added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useEffect } from 'react';
+import { StatusBar, Alert } from 'react-native';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import NetInfo from '@react-native-community/netinfo';
 
 import RootStack from './navigation';
 
 const App = () => {
+  useEffect(() => {
+    const netUnsubscribe = NetInfo.addEventListener(networkState => {
+      if (!networkState.isConnected) {
+        Alert.alert('Network error', 'Connection lost');
+      }
+    });
+    return () => {
+      netUnsubscribe();
+    };
+  }, []);
+
   return (
     <SafeAreaProvider>
       <StatusBar barStyle="light-content" />
